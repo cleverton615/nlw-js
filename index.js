f5e53972ad0c91f27fa5f5409b30419408b0e531
CLEVERTON ALLAN DE MELO <CLEVERTON_ALLAN@OUTLOOK.COM>
2024-09-14 01:00:09 -0300

    listar metas

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { select, input } = require('@inquirer/prompts') // Importanção do modulo require
+const { select, input, checkbox } = require('@inquirer/prompts') // Importanção do modulo require
 
 let meta = {
     value: 'Tomar 3L de água por dia',
@@ -18,6 +18,33 @@ const cadastrarMeta = async () => {
     metas.push(
         { value: meta, checked: false }
     )
+}
+
+const listarMetas = async () => {
+    const respostas = await checkbox({
+        message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o enter para finalizar essa etapa",
+        choices: [...metas], //jogando tudo que tem em metas (spread)
+        instructions: false
+    })
+
+    if(respostas.length == 0){
+        console.log("Nenhuma meta selecionada!")
+        return
+    }
+
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
+    respostas.forEach((resposta) => { //forEach(paraCada)
+        const meta = metas.find((m) => {
+            return m.value == resposta
+        })
+
+        meta.checked = true
+    })
+
+    console.log('Meta(s) marcadas como concluída(s)')
 }
 
 const start = async () => { // ArrowFunction
@@ -47,7 +74,7 @@ const start = async () => { // ArrowFunction
                 console.log(metas)
                 break
             case "listar":
-                console.log("Vamos listar")
+                await listarMetas()
                 break
             case "sair":
                 console.log("Até logo!")

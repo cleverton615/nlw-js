3d6708fbe195148983c3e35812d015fc5719e7ae
CLEVERTON ALLAN DE MELO <CLEVERTON_ALLAN@OUTLOOK.COM>
2024-09-14 00:07:37 -0300

    menu da aplicação

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-const { select } = require('@inquirer/prompts')
+const { select } = require('@inquirer/prompts') // Importanção do modulo require
+
+const start = async () => { // ArrowFunction
+
+    while(true){
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
+
+        switch(opcao) {
+            case "cadastrar":
+                console.log("Vamos cadastrar")
+                break
+            case "listar":
+                console.log("Vamos listar")
+                break
+            case "sair":
+                console.log("Até logo!")
+                return     
+        }
+    }
+}
+
+start()

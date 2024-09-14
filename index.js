9e463c016b8649ae85855231e28341daade98540
CLEVERTON ALLAN DE MELO <CLEVERTON_ALLAN@OUTLOOK.COM>
2024-09-14 15:12:22 -0300

    adiciona metas abertas

--- index.js
+++ index.js
@@ -57,8 +57,24 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        message: "Metas Realizadas",
+        message: "Metas Realizadas " + realizadas.length,
         choices: [...realizadas]
+    })
+}
+
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+    return !meta.checked  //inverte o valor de um boolean  
+    })
+
+    if(abertas.length ==0){
+        console.log("Não existem metas abertas!")
+        return
+    }
+
+    await select({
+        message: "Metas Abertas " + abertas.length,
+        choices: [...abertas]
     })
 }
 
@@ -81,6 +97,10 @@ const start = async () => { // ArrowFunction
                     value: "realizadas"
                 },
                 {
+                    name: "Metas abertas",
+                    value: "abertas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -97,6 +117,9 @@ const start = async () => { // ArrowFunction
                 break
             case "realizadas":
                 await metasRealizadas()    
+                break
+            case "abertas":
+                await metasAbertas()    
                 break
             case "sair":
                 console.log("Até logo!")
